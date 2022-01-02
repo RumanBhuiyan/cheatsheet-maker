825d3e0da08a3249827eec3e6e9b7e6e06ab6aee
Ruman Bhuiyan <ruman.bhuiyan@dsinnovators.com>
2022-01-02 20:13:47 +0600

    add page middle image

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Logo from "./Components/Logo";
+import PageImage from './Components/PageImage'
 
 export default function Home() {
   return (
     <div >
      <Logo />
+     <PageImage />
     </div>
   )
 }
